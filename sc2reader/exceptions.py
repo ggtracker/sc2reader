f632da6ce206dddb76f7e347800e6ff6d9182378
Graylin Kim <graylin.kim@gmail.com>
2011-02-27 11:19:15 -0500

    Adds in some basic __str__ functionality to have printable exceptions

--- sc2reader/exceptions.py
+++ sc2reader/exceptions.py
@@ -4,3 +4,11 @@ class ParseError(Exception):
         self.replay = replay
         self.event = event
         self.bytes = bytes
+        
+    def __str__(self):
+        return """ParseError %s
+            %s - %s
+            %s""" % (self.message, self.event.type, self.event.code, self.bytes)
+        
+    def __repr__(self):
+        return str(self)
