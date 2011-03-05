530000943a3dc4e60bc0edf4aa48da2ef539a75e
Graylin Kim <graylin.kim@gmail.com>
2011-03-04 21:43:44 -0500

    Adds an event 0x02 - 0x0E which appeared in a footman warz replay. The replay still does not yet pass though, appears to be a common event in this game type

--- sc2reader/parsers.py
+++ sc2reader/parsers.py
@@ -222,7 +222,8 @@ class EventParser(object):
             (ResourceTransferEventParser(), lambda e: e.code & 0x0F == 0xF and e.code >> 4 <= 0x9 ),],
         0x02: [
             (UnknownEventParser_0206(), lambda e: e.code == 0x06 ),
-            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),],
+            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),
+            (UnknownEventParser_020E(), lambda e: e.code == 0x0E ),],
         0x03: [
             (CameraMovementEventParser_87(), lambda e: e.code == 0x87 ),
             (CameraMovementEventParser_08(), lambda e: e.code == 0x08 ),
@@ -245,7 +246,6 @@ class EventParser(object):
         while bytes.remaining > 0:
             #Save the start so we can trace for debug purposes
             start = bytes.cursor
-            
             
             #First section is always a timestamp marking the elapsed time
             #since the last eventObjectlisted
@@ -271,6 +271,7 @@ class EventParser(object):
                 end = bytes.cursor
                 bytes.stream.seek(start)
                 event.bytes = bytes.get_bytes(end-start)
+
             except TypeError as e:
                 raise #ParseError(e.message, replay, event, bytes)
             
@@ -298,7 +299,8 @@ class EventParser_16561(EventParser):
             (ResourceTransferEventParser_16561(), lambda e: e.code & 0x0F == 0xF and e.code >> 4 <= 0x8 ),],
         0x02: [
             (UnknownEventParser_0206(), lambda e: e.code == 0x06 ),
-            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),],
+            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),
+            (UnknownEventParser_020E(), lambda e: e.code == 0x0E ),],
         0x03: [
             (CameraMovementEventParser_87(), lambda e: e.code == 0x87 ),
             (CameraMovementEventParser_08(), lambda e: e.code == 0x08 ),
@@ -338,7 +340,8 @@ class EventParser_17326(EventParser):
             (ResourceTransferEventParser_16561(), lambda e: e.code & 0x0F == 0xF and e.code >> 4 <= 0x9 ),],
         0x02: [
             (UnknownEventParser_0206(), lambda e: e.code == 0x06 ),
-            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),],
+            (UnknownEventParser_0207(), lambda e: e.code == 0x07 ),
+            (UnknownEventParser_020E(), lambda e: e.code == 0x0E ),],
         0x03: [
             (CameraMovementEventParser_87(), lambda e: e.code == 0x87 ),
             (CameraMovementEventParser_08(), lambda e: e.code == 0x08 ),
