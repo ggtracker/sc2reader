5b0d5fa260d0c49379ad62f2a4e977dc31b68ff0
Graylin Kim <graylin.kim@gmail.com>
2011-03-16 00:45:48 -0400

    Makes OrderedDict import from ordereddict if it isn't present in the collections module. Also adds a new configuration option for parsing a replay while skiping the events information.

--- sc2reader/config.py
+++ sc2reader/config.py
@@ -1,4 +1,7 @@
-from ordereddict import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 
 from sc2reader.objects import Replay
 from sc2reader.processors import *
@@ -25,3 +28,20 @@ class DefaultConfig(object):
                 ApmProcessor,
                 ResultsProcessor
             ]
+
+class NoEventsConfig(object):
+    def __init__(self):
+        self.ReplayClass = Replay
+        self.readers = OrderedDict()
+        self.readers['replay.initData'] = [ ReplayInitDataReader() ]
+        self.readers['replay.details'] = [ ReplayDetailsReader() ]
+        self.readers['replay.attributes.events'] = [ AttributeEventsReader_17326(), AttributeEventsReader() ]
+        self.readers['replay.message.events'] = [ MessageEventsReader() ]
+
+        self.processors = [
+            PeopleProcessor,
+            AttributeProcessor,
+            TeamsProcessor,
+            MessageProcessor,
+            RecorderProcessor,
+        ]
