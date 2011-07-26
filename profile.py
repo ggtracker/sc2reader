dd544d868110e58610a8e865f49a6d385da64fdb
Graylin Kim <graylin.kim@gmail.com>
2011-07-26 02:04:55 -0400

    Updates profile.py to the new interfaces

--- profile.py
+++ profile.py
@@ -1,27 +1,11 @@
-import cProfile
-from pstats import Stats
-import time
+import cProfile, pstats, time
 
-import os,sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import sc2reader
-
-skipnames = ('empty','footman')
 
 def parse_replays():
     # Run four times to dampen noise
     for run in range(1,4):
-        file_list = []
-        rootdir = "test_replays/build17811/"
-        for root, sub_folders, files in os.walk(rootdir):
-            for file in files:
-                basename, extension = os.path.splitext(file)
-                if (basename not in skipnames and extension.lower() == ".sc2replay"):
-                    file_list.append(os.path.join(root,file))
-
-        for file in file_list:
-            print file
-            replay = sc2reader.read(file)
+        sc2reader.read("test_replays/1.3.0.18092",verbose=True)
 
 # Use the results of this function when comparing performance with other libraries.
 def benchmark_with_timetime():
@@ -31,10 +15,11 @@ def benchmark_with_timetime():
     print diff
 
 def profile():
-    cProfile.run("parse_replays()","replay_profile")
-    stats = Stats("replay_profile")
-    stats.strip_dirs().sort_stats("time").print_stats(30)  
+    cProfile.run("parse_replays()","replay_profile2")
+    stats = pStats.Stats("replay_profile")
+    stats.strip_dirs().sort_stats("time").print_stats(30)
 
 
-#benchmark_with_timetime()
-profile()
+if __name__ == '__main__':
+    #benchmark_with_timetime()
+    profile()
