c1d1125ecc91beba0eb8f0f04594800c2140f4f7
Graylin Kim <graylin.kim@gmail.com>
2011-08-07 19:14:32 -0400

    print ReadError now has useful output.

--- sc2reader/exceptions.py
+++ sc2reader/exceptions.py
@@ -15,6 +15,9 @@ class ReadError(SC2ReaderError):
         self.__dict__.update(locals())
         super(ReadError, self).__init__(msg)
 
+    def __str__(self):
+        return "{0}, Type: {1}, Code: {2}".format(self.msg, self.type, self.code)
+
 class ProcessError(SC2ReaderError):
     pass
 
