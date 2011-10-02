82b44e8b7e5d790fdbe98512a44b048e8fc512ba
Graylin Kim <graylin.kim@gmail.com>
2011-10-02 12:01:52 -0400

    Make the configuration use absolute_import as well

--- sc2reader/config.py
+++ sc2reader/config.py
@@ -1,6 +1,7 @@
-from .processors import *
-from .readers import *
-from .utils import AttributeDict, RangeMap
+from __future__ import absolute_import
+
+from sc2reader import readers as r
+from sc2reader.utils import AttributeDict, RangeMap
 
 ALL_FILES = [
     'replay.initData',
@@ -53,41 +54,41 @@ default_options = AttributeDict(
 
 readers = RangeMap()
 readers.add_range(0, 16561, {
-        'replay.initData': InitDataReader(),
-        'replay.details': DetailsReader(),
-        'replay.attributes.events': AttributeEventsReader(),
-        'replay.message.events': MessageEventsReader(),
-        'replay.game.events': GameEventsReader(),
+        'replay.initData': r.InitDataReader(),
+        'replay.details': r.DetailsReader(),
+        'replay.attributes.events': r.AttributeEventsReader(),
+        'replay.message.events': r.MessageEventsReader(),
+        'replay.game.events': r.GameEventsReader(),
     })
 
 readers.add_range(16561, 17326, {
-        'replay.initData': InitDataReader(),
-        'replay.details': DetailsReader(),
-        'replay.attributes.events': AttributeEventsReader(),
-        'replay.message.events': MessageEventsReader(),
-        'replay.game.events': GameEventsReader_16561(),
+        'replay.initData': r.InitDataReader(),
+        'replay.details': r.DetailsReader(),
+        'replay.attributes.events': r.AttributeEventsReader(),
+        'replay.message.events': r.MessageEventsReader(),
+        'replay.game.events': r.GameEventsReader_16561(),
     })
 
 readers.add_range(17326, 18574, {
-        'replay.initData': InitDataReader(),
-        'replay.details': DetailsReader(),
-        'replay.attributes.events': AttributeEventsReader_17326(),
-        'replay.message.events': MessageEventsReader(),
-        'replay.game.events': GameEventsReader_16561(),
+        'replay.initData': r.InitDataReader(),
+        'replay.details': r.DetailsReader(),
+        'replay.attributes.events': r.AttributeEventsReader_17326(),
+        'replay.message.events': r.MessageEventsReader(),
+        'replay.game.events': r.GameEventsReader_16561(),
     })
 
 readers.add_range(18574, 19595, {
-        'replay.initData': InitDataReader(),
-        'replay.details': DetailsReader(),
-        'replay.attributes.events': AttributeEventsReader_17326(),
-        'replay.message.events': MessageEventsReader(),
-        'replay.game.events': GameEventsReader_18574(),
+        'replay.initData': r.InitDataReader(),
+        'replay.details': r.DetailsReader(),
+        'replay.attributes.events': r.AttributeEventsReader_17326(),
+        'replay.message.events': r.MessageEventsReader(),
+        'replay.game.events': r.GameEventsReader_18574(),
     })
 
 readers.add_range(19595, None, {
-        'replay.initData': InitDataReader(),
-        'replay.details': DetailsReader(),
-        'replay.attributes.events': AttributeEventsReader_17326(),
-        'replay.message.events': MessageEventsReader(),
-        'replay.game.events': GameEventsReader_19595(),
+        'replay.initData': r.InitDataReader(),
+        'replay.details': r.DetailsReader(),
+        'replay.attributes.events': r.AttributeEventsReader_17326(),
+        'replay.message.events': r.MessageEventsReader(),
+        'replay.game.events': r.GameEventsReader_19595(),
     })
