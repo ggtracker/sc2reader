6ed83ce0db86c0d25b0f64918be028e80eb60d12
Graylin Kim <graylin.kim@gmail.com>
2011-10-05 00:25:48 -0400

    Add templated data files.

---
+++ protoss.py
@@ -0,0 +1,42 @@
+pylon
+
+Mode( )
+gateway/warpgate
+
+cyberneticscore
+dark shrine
+themplar archives
+twilight council
+robotics bay
+robotics facility
+stargate
+Fleet Beacon
+
+class Nexus(BaseData.Nexus):
+    produces = {
+        0x0210100: 'Train Probe',
+    }
+Forge
+Photon Cannon
+
+
+Probe
+Zealot
+Stalker
+High Templar
+Dark Templar
+Archon
+Sentry
+Immortal
+Observer
+Colossus
+
+@Mode('Phasing')
+Warp Prism
+
+@Channels
+Pheonix
+
+Carrier
+Void Ray
+Mothership
