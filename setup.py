5a80a1401b2a58340730dec25662ebae8d893175
Graylin Kim <graylin.kim@gmail.com>
2012-01-30 23:41:30 -0500

    Fix spacing inconsistencies and add zip_safe flag.

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 import setuptools, sys
 
 setuptools.setup(
-	name="sc2reader",
-	version='0.3.3',
-	license="MIT",
+    name="sc2reader",
+    version='0.3.3',
+    license="MIT",
 
-	author="Graylin Kim",
-	author_email="graylin.kim@gmail.com",
-	url="https://github.com/GraylinKim/sc2reader",
+    author="Graylin Kim",
+    author_email="graylin.kim@gmail.com",
+    url="https://github.com/GraylinKim/sc2reader",
 
-	description="Utility for parsing Starcraft II replay files",
-	long_description=open("README.txt").read(),
-	keywords=["starcraft 2","sc2","parser","replay"],
-	classifiers=[
-			"Environment :: Console",
-			"Development Status :: 4 - Beta",
-			"Programming Language :: Python",
-			"Programming Language :: Python :: 2.7",
-			"Intended Audience :: Developers",
-			"License :: OSI Approved :: MIT License",
-			"Natural Language :: English",
-			"Operating System :: OS Independent",
-			"Environment :: Other Environment",
-			"Topic :: Utilities",
-			"Topic :: Software Development :: Libraries",
-			"Topic :: Games/Entertainment :: Real Time Strategy",
-		],
-	entry_points={
+    description="Utility for parsing Starcraft II replay files",
+    long_description=open("README.txt").read(),
+    keywords=["starcraft 2","sc2","parser","replay"],
+    classifiers=[
+            "Environment :: Console",
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Environment :: Other Environment",
+            "Topic :: Utilities",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Games/Entertainment :: Real Time Strategy",
+        ],
+    entry_points={
         'console_scripts': [
             'sc2autosave = sc2reader.scripts.sc2autosave:main',
             'sc2printer = sc2reader.scripts.sc2printer:main',
@@ -34,6 +34,7 @@ setuptools.setup(
             'sc2replayer = sc2reader.scripts.sc2replayer:main',
         ]
     },
-	install_requires=['mpyq','argparse'] if float(sys.version[:3]) < 2.7 else ['mpyq'],
-	packages=['sc2reader', 'sc2reader.scripts', 'sc2reader.processors', 'sc2reader.data'],
+    install_requires=['mpyq','argparse'] if float(sys.version[:3]) < 2.7 else ['mpyq'],
+    packages=['sc2reader', 'sc2reader.scripts', 'sc2reader.processors', 'sc2reader.data'],
+    zip_safe=True
 )
