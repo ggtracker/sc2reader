5aa7b7c0759fbf520f44fbc538654c448f47dd36
Graylin Kim <graylin.kim@gmail.com>
2012-04-06 10:14:58 -0400

    Add new listeners, primarily used for debugging.

--- sc2reader/listeners/__init__.py
+++ sc2reader/listeners/__init__.py
@@ -4,3 +4,5 @@ from collections import defaultdict
 
 from sc2reader.listeners.apm import APMTracker
 from sc2reader.listeners.selection import SelectionListener
+from sc2reader.listeners.location import LocationListener
+from sc2reader.listeners.target import TargetListener
