48a327a8925176c64b7c141ff79c18ffed0daeda
David Joerg <dsjoerg@gmail.com>
2012-08-01 12:13:14 -0400

    rearrange words, add a header and error message to the output

--- game_events_debug.py
+++ game_events_debug.py
@@ -13,22 +13,16 @@
           0x000F5 00:05 P2 SelectionEvent      - 00 22 ac 00 02 f4 00 00  | 00 0c a1 89 00 10 00 00 00 21 0b 01 00 01 5c 01 00 0c 22 0b
                                                  01 08 34 07 00 23 80 00 01 1f 0d 41 18 00 8a c0 80 00 05 ff 00 08 21 ac 00 02 11 01 02
 
-    The first line is the full path to the file followed by an indented list
-    of the last 2 events to be parsed with their associated bytes.
+    The first line is the full path to the file, followed by an
+    indented list of the last 3 events that were parsed before an
+    Error was thrown.
 
-    The 3rd line for the event the parser failed on is in the following format:
+    The last line shows the last event that was parsed without
+    throwing an Error, and the bytes that follow it.  The pipe
+    character delimits where the parser ended the event.
 
-        cursor event_type - event_bytes | following bytes
-                            and the next bytes after that
-
-    With a pipe character delimiting where the parser ended the event.
-
-    The general workflow is something like this:
-
-        1) Run the debugger over a set of replays
-        2) Figure out where the failed event boundry should have been
-        3) Alter the reader code to make it happen
-        4) Repeat
+    Often an Error is thrown because our code consumes the wrong
+    number of bytes for the immediately previous Event.
 
     In the example above:
 
@@ -41,6 +35,15 @@
     So the correct bytes for the failed event are these:
 
         00 22 ac 00 02 f4 00 00 00
+
+
+    The general workflow is something like this:
+
+        1) Run the debugger over a set of replays
+        2) Figure out where the failed event boundry should have been
+        3) Alter the reader code to make it happen
+        4) Repeat
+
 
     To figure out how to alter the reader code you'll probably need to compile
     lists of failed events with these correct bytes and look for patterns that
@@ -102,6 +105,7 @@ for filename in set(sum((list(get_files(arg)) for arg in sys.argv[1:]),list())):
     except ReadError as e:
         print filename
         if len(e.game_events):
+            print " Location  Time P# EventType             Bytes"
             for event in e.game_events[-3:-1]:
                 print "  ....... {:0>2}:{:0>2} P{} {: <19} - {}".format(event.second/60, event.second%60, event.pid, get_name(event), format_bytes(event.bytes))
             last_event = e.game_events[-1]
@@ -110,6 +114,7 @@ for filename in set(sum((list(get_files(arg)) for arg in sys.argv[1:]),list())):
             line2 = e.buffer.read_range(line1_end,line1_end+BYTES_WIDTH)
             print "  0x{:0>5X} {:0>2}:{:0>2} P{} {: <19} - {} | {}".format(e.location, last_event.second/60, last_event.second%60, last_event.pid, get_name(last_event), format_bytes(last_event.bytes), format_bytes(line1))
             print " "*41+format_bytes(line2)
+            print e
         else:
             print "  0x{:0>5X} {: <19} - {}".format(0, "None", format_bytes(e.buffer.read_range(0,BYTES_WIDTH+1)))
         print
