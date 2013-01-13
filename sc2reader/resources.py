8824160dddeae0118cc972c66dac1a6e3dc2d0b0
Graylin Kim <graylin.kim@gmail.com>
2013-01-13 17:38:42 -0500

    Fix the sc2boprinter script up.

--- sc2reader/resources.py
+++ sc2reader/resources.py
@@ -1022,7 +1022,7 @@ class GameSummary(Resource):
 
     def __str__(self):
         return "{0} - {1} {2}".format(self.start_time,self.game_length,
-                                         'v'.join(''.join(self.players[p].race[0] for p in self.teams[tid]) for tid in self.teams))
+                                         'v'.join(''.join(self.players[p.pid].play_race[0] for p in self.teams[tid]) for tid in self.teams))
 
 
 
