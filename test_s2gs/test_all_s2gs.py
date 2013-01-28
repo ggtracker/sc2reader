0d392d4e0e389c5087846b64619405c36882c3c9
David Joerg <dsjoerg@gmail.com>
2013-01-28 11:01:14 -0500

    and add a HotS s2gs test that currently passes

--- test_s2gs/test_all_s2gs.py
+++ test_s2gs/test_all_s2gs.py
@@ -10,5 +10,8 @@ sys.path.insert(0, os.path.normpath(os.path.join(os.path.dirname(os.path.abspath
 import sc2reader
 from sc2reader.exceptions import ParseError
 
-def test_an_s2gs():
+def test_a_WoL_s2gs():
     summary = sc2reader.load_game_summary("test_s2gs/s2gs1.s2gs")
+
+def test_a_HotS_s2gs():
+    summary = sc2reader.load_game_summary("test_s2gs/hots1.s2gs")
