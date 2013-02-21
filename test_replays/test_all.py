8bd31023b76f13783b337db67ee4c31211ebf6e8
David Joerg <dsjoerg@gmail.com>
2013-02-21 11:25:56 -0500

    get clantag breaking test to pass. upgrade WoL 2.0.4 to use DetailsReader_Beta_24764 instead of DetailsReader_22612 despite not having a breaking test for that change. all tests pass now

--- test_replays/test_all.py
+++ test_replays/test_all.py
@@ -222,3 +222,7 @@ def test_oracle_parsing():
 def test_clan_players():
     replay = sc2reader.load_replay("test_replays/2.0.4.24944/Lunar Colony V.SC2Replay")
     assert len(replay.people) == 4
+
+def test_WoL_204():
+    replay = sc2reader.load_replay("test_replays/2.0.4.24944/ggtracker_1789768.SC2Replay")
+    assert len(replay.people) == 2
