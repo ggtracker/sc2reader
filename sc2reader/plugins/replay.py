2ab31afd2ee19538e74a38818401c7968cb8da34
Graylin Kim <graylin.kim@gmail.com>
2013-05-26 17:20:37 -0400

    Clean up and document the new APMTracker.
    closes #106

--- sc2reader/plugins/replay.py
+++ sc2reader/plugins/replay.py
@@ -90,22 +90,29 @@ def toDict(replay):
 
 @plugin
 def APMTracker(replay):
+    """
+    Builds ``player.aps`` and ``player.apm`` dictionaries where an action is
+    any Selection, Hotkey, or Ability event.
+
+    Also provides ``player.avg_apm`` which is defined as the sum of all the
+    above actions divided by the number of seconds played by the player (not
+    necessarily the whole game) multiplied by 60.
+    """
     for player in replay.players:
         player.aps = defaultdict(int)
         player.apm = defaultdict(int)
-        player.seconds_played = replay.length.seconds
+        seconds_played = replay.length.seconds
 
         for event in player.events:
             if event.name == 'SelectionEvent' or 'AbilityEvent' in event.name or 'Hotkey' in event.name:
                 player.aps[event.second] += 1
                 player.apm[event.second/60] += 1
 
-            if event.name == 'PlayerLeaveEvent':
-                player.seconds_played = event.second
-                break
+            elif event.name == 'PlayerLeaveEvent':
+                seconds_played = event.second
 
         if len(player.apm) > 0:
-            player.avg_apm = sum(player.apm.values())/float(player.seconds_played)*60
+            player.avg_apm = sum(player.aps.values())/float(seconds_played)*60
         else:
             player.avg_apm = 0
 
