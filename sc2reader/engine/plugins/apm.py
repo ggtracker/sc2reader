90c5c9db788c0450483d71c38155fcf0a9d56220
Graylin Kim <graylin.kim@gmail.com>
2013-06-02 08:40:04 -0400

    Fix the engine's APM plugin and add some documentation.

--- sc2reader/engine/plugins/apm.py
+++ sc2reader/engine/plugins/apm.py
@@ -1,6 +1,16 @@
 from collections import Counter
 
 class APMTracker(object):
+    """
+    Builds ``player.aps`` and ``player.apm`` dictionaries where an action is
+    any Selection, Hotkey, or Ability event.
+
+    Also provides ``player.avg_apm`` which is defined as the sum of all the
+    above actions divided by the number of seconds played by the player (not
+    necessarily the whole game) multiplied by 60.
+
+    APM is 0 for games under 1 minute in length.
+    """
 
     def handleInitGame(self, event, replay):
         for player in replay.players:
@@ -19,6 +29,6 @@ class APMTracker(object):
         print "Handling End Game"
         for player in replay.players:
             if len(player.apm.keys()) > 0:
-                player.avg_apm = sum(player.apm.values())/float(player.seconds_played)*60
+                player.avg_apm = sum(player.aps.values())/float(player.seconds_played)*60
             else:
                 player.avg_apm = 0
