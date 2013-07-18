c51cf080331a43003ac2131fdd4cc8bd18a96ce7
Graylin Kim <graylin.kim@gmail.com>
2013-07-18 09:52:28 -0400

    Expose indent and encoding parameters on the cli.

--- sc2reader/scripts/sc2json.py
+++ sc2reader/scripts/sc2json.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
 import sc2reader
 from sc2reader.plugins.replay import toJSON
 
-def main():
-    factory = sc2reader.factories.SC2Factory()
-    factory.register_plugin("Replay", toJSON())
 
-    replay_json = factory.load_replay(sys.argv[1])
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Prints replay data to a json string.")
+    parser.add_argument('--indent', '-i', type=int, default=None, help="The per-line indent to use when printing a human readable json string")
+    parser.add_argument('--encoding', '-e', type=str, default='UTF-8', help="The character encoding use..")
+    parser.add_argument('path', metavar='path', type=str, nargs=1, help="Path to the replay to serialize.")
+    args = parser.parse_args()
+
+    factory = sc2reader.factories.SC2Factory()
+    factory.register_plugin("Replay", toJSON(encoding=args.encoding, indent=args.indent))
+    replay_json = factory.load_replay(args.path[0])
     print replay_json
 
 if __name__ == '__main__':
