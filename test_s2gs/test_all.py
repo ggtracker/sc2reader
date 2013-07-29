ab12b1e61fa14555c820380321ba185dab7edd6e
Graylin Kim <graylin.kim@gmail.com>
2013-07-29 14:00:29 -0400

    Take 4.

--- test_s2gs/test_all.py
+++ test_s2gs/test_all.py
@@ -2,7 +2,7 @@
 
 # Newer unittest features aren't built in for python 2.6
 import sys
-if sys.version_info[:2] <= (2, 7):
+if sys.version_info[:2] < (2, 7):
     import unittest2 as unittest
 else:
     import unittest
