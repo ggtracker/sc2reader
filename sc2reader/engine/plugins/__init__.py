3d1bd6452e3a1bdb32d810f1b4f53ca94eb9f7de
jonomon <jono.chung@gmail.com>
2013-07-31 22:04:36 -0400

    added new creepTracker, supplyTracker  and tests

--- sc2reader/engine/plugins/__init__.py
+++ sc2reader/engine/plugins/__init__.py
@@ -5,3 +5,5 @@ from sc2reader.engine.plugins.apm import APMTracker
 from sc2reader.engine.plugins.selection import SelectionTracker
 from sc2reader.engine.plugins.context import ContextLoader
 from sc2reader.engine.plugins.gameheart import GameHeartNormalizer
+from sc2reader.engine.plugins.supply import SupplyTracker
+from sc2reader.engine.plugins.creeptracker import CreepTracker
