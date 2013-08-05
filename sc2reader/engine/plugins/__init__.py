01ec670067796f22358f42b02f3b1161c28ec90d
Kevin Leung <kkleung89@gmail.com>
2013-08-05 00:14:34 -0400

    GameHeart Plugin

--- sc2reader/engine/plugins/__init__.py
+++ sc2reader/engine/plugins/__init__.py
@@ -4,3 +4,4 @@ from __future__ import absolute_import, print_function, unicode_literals, divisi
 from sc2reader.engine.plugins.apm import APMTracker
 from sc2reader.engine.plugins.selection import SelectionTracker
 from sc2reader.engine.plugins.context import ContextLoader
+from sc2reader.engine.plugins.gameheart import GameHeartNormalizer
