9f6b076f7ba5a7660258d0027f7b51fc4ded0baf
Graylin Kim <graylin.kim@gmail.com>
2013-08-07 01:11:52 -0400

    Oops. Forgot the events file.

---
+++ sc2reader/engine/events.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals, division
+
+
+class InitGameEvent(object):
+    name = 'InitGame'
+
+
+class EndGameEvent(object):
+    name = 'EndGame'
+
+
+class PluginExit(object):
+    name = 'PluginExit'
+
+    def __init__(self, plugin, code=0, details=None):
+        self.plugin = plugin
+        self.code = code
+        self.details = details or {}
