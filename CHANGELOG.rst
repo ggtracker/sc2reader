e3ff027ae1d77c1d685d91c4816f4f81c6f2bd54
Graylin Kim <graylin.kim@gmail.com>
2013-12-10 22:46:19 -0500

    Use generic UnitType and Ability classes for data.
    The dynamically created classes don't play well with pickle and
    were unncessarily complex. The only change here is that you can't
    use this anymore.
    
      unit._type_class.__class__.__name__
    
    Instead you can use the shorter:
    
      unit._type_class.name
    
    No problem.
    
    Conflicts:
    	CHANGELOG.rst

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,6 +5,7 @@ CHANGELOG
 0.6.5 - December ?? 2013
 ---------------------------
 
+* Use generic UnitType and Ability classes for data. This means no more unit._type_class.__class__.__name__. But hopefully people were not doing that anyway.
 * Removed the defunct replay.player_names attribute.
 * Removed the defunct replay.events_by_type attribute.
 * Removed the defunct replay.other_people attribute.
--- sc2reader/data/__init__.py
+++ sc2reader/data/__init__.py
@@ -33,10 +33,7 @@ train_commands = json.loads(command_data)
 
 
 class Unit(object):
-    """
-    Represents an in-game unit.
-    """
-
+    """Represents an in-game unit."""
     def __init__(self, unit_id):
         #: A reference to the player that currently owns this unit. Only available for 2.0.8+ replays.
         self.owner = None
@@ -195,22 +192,64 @@ class Unit(object):
         return str(self)
 
 
+class UnitType(object):
+    """ Represents an in game unit type """
+    def __init__(self, type_id, str_id=None, name=None, title=None, race=None, minerals=0,
+                 vespene=0, supply=0, is_building=False, is_worker=False, is_army=False):
+        #: The internal integer id representing this unit type
+        self.id = type_id
+
+        #: The internal string id representing this unit type
+        self.str_id = str_id
+
+        #: The name of this unit type
+        self.name = name
+
+        #: The printable title of this unit type; has spaces and possibly punctuation
+        self.title = title
+
+        #: The race this unit type belongs to
+        self.race = race
+
+        #: The mineral cost of this unit type
+        self.minerals = minerals
+
+        #: The vespene cost of this unit type
+        self.vespene = vespene
+
+        #: The supply cost of this unit type
+        self.supply = supply
+
+        #: Boolean flagging this unit type as a building
+        self.is_building = is_building
+
+        #: Boolean flagging this unit type as a worker
+        self.is_worker = is_worker
+
+        #: Boolean flagging this unit type as an army unit
+        self.is_army = is_army
+
+
 class Ability(object):
+    """ Represents an in-game ability """
+    def __init__(self, id, name=None, title=None, is_build=False, build_time=0, build_unit=None):
+        #: The internal integer id representing this ability.
+        self.id = id
 
-    #: The internal integer id representing this ability.
-    id = None
+        #: The name of this ability
+        self.name = name
 
-    #: The name of this ability
-    name = ""
+        #: The printable title of this ability; has spaces and possibly punctuation.
+        self.title = title
 
-    #: Boolean flagging this ability as creating a new unit.
-    is_build = False
+        #: Boolean flagging this ability as creating a new unit.
+        self.is_build = is_build
 
-    #: The number of seconds required to build this unit. 0 if not ``is_build``.
-    build_time = 0
+        #: The number of seconds required to build this unit. 0 if not ``is_build``.
+        self.build_time = build_time
 
-    #: A reference to the :class:`Unit` type built by this ability. None if not ``is_build``.
-    build_unit = None
+        #: A reference to the :class:`UnitType` type built by this ability. Default to None.
+        self.build_unit = build_unit
 
 
 @loggable
@@ -260,20 +299,20 @@ class Build(object):
             self.logger.error("Unable to change type of {0} to {1} [frame {2}]; unit type not found in build {3}".format(unit, new_type, frame, self.id))
 
     def add_ability(self, ability_id, name, title=None, is_build=False, build_time=None, build_unit=None):
-        ability = type(str(name), (Ability,), dict(
-            id=ability_id,
+        ability = Ability(
+            ability_id,
             name=name,
             title=title or name,
             is_build=is_build,
             build_time=build_time,
             build_unit=build_unit
-        ))
+        )
         setattr(self, name, ability)
         self.abilities[ability_id] = ability
 
     def add_unit_type(self, type_id, str_id, name, title=None, race='Neutral', minerals=0, vespene=0, supply=0, is_building=False, is_worker=False, is_army=False):
-        unit = type(str(name), (Unit,), dict(
-            id=type_id,
+        unit = UnitType(
+            type_id,
             str_id=str_id,
             name=name,
             title=title or name,
