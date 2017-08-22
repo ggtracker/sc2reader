2ecd0b397a79471b96cf6c44cc118e550e629e76
cclauss <cclauss@bluewin.ch>
2017-08-22 17:22:04 +0200

    print() function for Python 3

--- new_units.py
+++ new_units.py
@@ -18,10 +18,10 @@ with open(sys.argv[1],'r') as new_units:
 	for line in new_units:
 		new_unit_name = line.strip().split(',')[1]
 		if new_unit_name not in UNIT_LOOKUP:
-			print "{0},{1}".format(new_unit_name,new_unit_name)
+			print("{0},{1}".format(new_unit_name,new_unit_name))
 
-print
-print
+print('')
+print('')
 
 ABIL_LOOKUP = dict()
 for entry in pkgutil.get_data('sc2reader.data', 'ability_lookup.csv').split('\n'):
@@ -33,4 +33,4 @@ with open(sys.argv[2], 'r') as new_abilities:
 	for line in new_abilities:
 		new_ability_name = line.strip().split(',')[1]
 		if new_ability_name not in ABIL_LOOKUP:
-			print "{0},{1}".format(new_ability_name,new_ability_name)
+			print("{0},{1}".format(new_ability_name,new_ability_name))
