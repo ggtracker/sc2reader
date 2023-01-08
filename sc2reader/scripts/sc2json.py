99e1071a2bd69cd8e485caf2c7a44b3444fb7f9a
manuelseeger <mail@manuelseeger.de>
2023-01-08 11:31:40 +0100

    Remove encoding option from JSON script.
    Python3 deprecates encoding option on json.dumps and defaults to UTF8.
    
    The existing try didn't work since the plugin wrapper
    doesn't call json.dumps until after except statement in line 41.

--- sc2reader/scripts/sc2json.py
+++ sc2reader/scripts/sc2json.py
@@ -2,6 +2,7 @@
 
 import sc2reader
 from sc2reader.factories.plugins.replay import toJSON
+import sys
 
 
 def main():
@@ -16,6 +17,13 @@ def main():
         help="The per-line indent to use when printing a human readable json string",
     )
     parser.add_argument(
+        "--encoding",
+        "-e",
+        type=str,
+        default="UTF-8",
+        help="The character encoding use..",
+    )
+    parser.add_argument(
         "path",
         metavar="path",
         type=str,
@@ -25,9 +33,12 @@ def main():
     args = parser.parse_args()
 
     factory = sc2reader.factories.SC2Factory()
-    try:
-        factory.register_plugin("Replay", toJSON(indent=args.indent))
-    except TypeError:
+
+    if sys.version_info.major < 3:
+        factory.register_plugin(
+            "Replay", toJSON(encoding=args.encoding, indent=args.indent)
+        )  # legacy Python
+    else:
         factory.register_plugin("Replay", toJSON(indent=args.indent))
     replay_json = factory.load_replay(args.path[0])
     print(replay_json)
